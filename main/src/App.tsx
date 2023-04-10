17bea371689fd7263f2ebe78b8e6371e123cd949
aminriyad <aminriyad@hotmail.com>
2023-04-10 03:08:19 -0500

    ytcy

--- main/src/App.tsx
+++ main/src/App.tsx
@@ -2,14 +2,9 @@ import { useState, useEffect } from "react"
 const App = () => {
     const [joke,setjoke]= useState("hey")
     const [end,setend]=useState("Click on me to make a joke")
-    const center ={
-        display:"grid",
-        justifyContent:"center",
-        margin:"0 auto"
-    }
+    const center ={display:"grid",justifyContent:"center",margin:"0 auto"}
     const getter = () =>{
-        console.log("Clicked")
-        fetch("https://v2.jokeapi.dev/joke/Dark?blacklistFlags=racist,sexist")
+        fetch("https://v2.jokeapi.dev/joke/Dark?blacklistFlags=racist,sexist&type=twopart")
         .then(async x =>{
             const res = await x.json()
             console.log(res)
@@ -18,7 +13,17 @@ const App = () => {
         })
         .catch(e => console.log(e, "is the error"))
 }
-    return (
+
+//underconstruction 
+//3:08am
+// const amin = (e:any) =>{
+// if(e.keyCode===13||"Enter"){
+//     getter()
+//     console.log("you clicked enter")
+// }
+// (e:any) => {e.keyCode === 13 && getter()
+// }
+return (
     <>
     <div style={center}>
     <div>{joke}</div>
@@ -28,5 +33,6 @@ const App = () => {
     </>
     );
 }
- 
+
+
 export default App;
